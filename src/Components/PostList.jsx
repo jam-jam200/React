add2c633f44c38edeb4cb17d956e84e9eeb4ba56
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-06 20:47:39 +0100

    reacting to react

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PostList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      errorMsg: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          posts: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          errorMsg: <div>Error in displaying list</div>,
+        });
+      });
+  }
+
   render() {
+    const { posts, errorMsg } = this.state;
+    const postlist = posts.map((post) => <div key={post.id}>{post.title}</div>);
     return (
       <div>
         <h1>List of posts</h1>
+        <h2>{postlist}</h2>
+        <h4>{errorMsg}</h4>
       </div>
     );
   }
